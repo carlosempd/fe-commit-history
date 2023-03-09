1dac6147bcdf5b5a514f0138c3e0e4c74631adca
Carlos <carlosempd@gmail.com>
2023-03-08 22:31:12 -0400

    configure new route and a wildcard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommitsComponent } from './pages/commits/commits.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'commits',
+    component: CommitsComponent
+  },
+  { 
+    path: '**', 
+    redirectTo: 'commits'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
