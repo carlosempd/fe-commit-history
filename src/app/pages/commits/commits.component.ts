da530c9e8312b8d7a8cc99215e6878ed1b3024f8
Carlos <carlosempd@gmail.com>
2023-03-08 20:13:20 -0400

    create commits component

---
+++ src/app/pages/commits/commits.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-commits',
+  templateUrl: './commits.component.html',
+  styleUrls: ['./commits.component.scss']
+})
+export class CommitsComponent {
+
+}
